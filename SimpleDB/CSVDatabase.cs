29feedc17c7df670c41b06fd049fd2fb964432e8
Guðmundur Jón <s25765@itu.dk>
2026-09-04 13:24:05 +0200

    read() and store() in the CSVDatabase.cs using generic type T
    Co-authored-by: Sarah Marie Christensen <sarahzebra@users.noreply.github.com>
    Co-authored-by: VladislavMiller <VladislavMiller@users.noreply.github.com>
    Co-authored-by: JulieNurnberger <JulieNurnberger@users.noreply.github.com>
    Co-authored-by: kirstineelvira19 <kirstineelvira19@users.noreply.github.com>

--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -1,7 +1,54 @@
 ﻿namespace SimpleDB;
+using CsvHelper;
+using CultureInfo = System.Globalization.CultureInfo;
 
 public sealed class CSVDatabase<T> : IDatabaseRepository<T>
 {
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        //read the CSV using StreamReader 
+        using (StreamReader reader = new StreamReader("bison_observe_cli_db.csv"))
+        using(var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            var records = csv.GetRecords<T>().ToList();
+            return records;
+
+            // IEnumerable<T> test = new List<T>();
+
+            // foreach (var record in records)
+            // {
+            //     Convert the Unix timestamp to the correct format
+            //     DateTimeOffset dateTime = DateTimeOffset.FromUnixTimeSeconds(record.timestamp);
+            //     string formattedDate = dateTime.ToString("MM/dd/yy HH:mm:ss");
+            //     test.Add(record.author);
+
+            //     Console.WriteLine($"{record.author} @ {formattedDate}: {record.message}");
+            // }
+            
+            // return author, date and message in array
+
+        }
+    }
+
+    public void Store(T record) 
+    {
+        using(StreamWriter writer = new StreamWriter("bison_observe_cli_db.csv", true)) //open the "book"
+        using(var csv = new CsvWriter(writer, CultureInfo.InvariantCulture)) //read the "page"
+        {
+            csv.WriteRecord(record);
+            csv.NextRecord();
+
+            // //taking in the message from the command line argument and storing the data correctly
+            // string message = args[1];
+            // string author = Environment.UserName;
+            // long unixTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            // var cheep = new Cheep (author, unixTimestamp, message);
+
+
+            // Console.WriteLine("Observation recorded.");
+        }
+    }
+
 
 }
 
--- SimpleDB/IDatabaseRepository.cs
+++ SimpleDB/IDatabaseRepository.cs
@@ -1,5 +1,5 @@
 namespace SimpleDB;
-interface IDatabaseRepository<T>
+public interface IDatabaseRepository<T> // added "public", because without it it's only visible within the SimpleDB project. 
 {
     public IEnumerable<T> Read(int? limit = null);
     public void Store(T record);
