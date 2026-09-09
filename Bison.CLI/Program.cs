15a185702f6dbd2c97bb07b3f59ad7e95ab0f603
Vladislav Miller <vlmi@itu.dk>
2026-09-09 14:52:44 +0200

    Added Commenting Feature
    Co-Authored-By: Sarah Marie Christensen <320209524+sarahzebra@users.noreply.github.com>

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -11,9 +11,18 @@ CSVDatabase<Comment> commentDatabase = new CSVDatabase<Comment>("comments.csv");
 //initalising the commands to use as well as their descriptions
 var readCommand = new Command("read","read all observations");
 var observeCommand = new Command("observe", "Record an observation");
-var messageArgument = new Argument<string>("message");
-observeCommand.Add(messageArgument);
+var commentCommand = new Command("comment", "Add a comment to an observation");
+var discussionCommand = new Command("discussion", "List all comments for a given observation");
 
+var messageArgument = new Argument<string>("message");
+var observationIdArgument = new Argument<int>("observation-id");
+var commentMessageArgument = new Argument<string>("comment-message");
+var discussionIdArgument = new Argument<int>("observation-id");
+
+observeCommand.Add(messageArgument);
+commentCommand.Add(commentMessageArgument);
+commentCommand.Add(observationIdArgument);
+discussionCommand.Add(discussionIdArgument);
 
 //establishing Root and subcommands "hierarchy"
 //Root command
@@ -22,7 +31,7 @@ observeCommand.Add(messageArgument);
 	    //- messageArgument
 var rootCommand = new RootCommand("RootCommand")
 {
-    Subcommands = {readCommand, observeCommand}
+    Subcommands = {readCommand, observeCommand, commentCommand, discussionCommand}
 };
 
 //read observations from the database
@@ -43,6 +52,28 @@ observeCommand.SetAction((ParseResult parseResult) =>
         observationDatabase.Store(observation); //actually store the observation
 
         UserInterface.PrintObsvervationRecorded(Message); //print conformation using User Interface
+});
+
+// comment command
+commentCommand.SetAction((ParseResult parseResult) =>
+{
+    int observationId = parseResult.GetRequiredValue(observationIdArgument);//get the matching observation Id
+    string message = parseResult.GetRequiredValue(commentMessageArgument); //get the message
+    string Author = Environment.UserName; //get the author
+    long unixTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds(); //get the time
+
+    var comment = new Comment(observationId, Author, message, unixTimestamp); //formatting the comment for storage
+    commentDatabase.Store(comment);//storing the comment in the matching comment database
+    UserInterface.PrintObsvervationRecorded(message); //print confirmation using User Interface
+});
+
+// discussion command
+discussionCommand.SetAction((ParseResult parseResult) =>
+{
+    int observationId = parseResult.GetRequiredValue(discussionIdArgument);//get the matching observation Id
+
+    IEnumerable<Comment> comments = commentDatabase.Read().Where(c => c.ObservationId == observationId); //get all comments matching the observation Id
+    UserInterface.PrintCheeps(comments);
 });
 
 //parses the input into a parseResult and invokes the action for the command
