93c1ad4041695b5b88c4e179b3ec42f6ee22253c
Guðmundur Jón <s25765@itu.dk>
2026-09-16 14:05:16 +0200

    StoreObservation test

--- test/BisonTest/EndToEndTest.cs
+++ test/BisonTest/EndToEndTest.cs
@@ -31,8 +31,10 @@ public class EndtoEndTest
         //string projectPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "..", "src","Bison.CLI"));
         process.StartInfo.Arguments = $"run --project ../../../../../src/Bison.CLI -- read";
 
+        //run command directly and capture what the program prints
         process.StartInfo.RedirectStandardOutput = true;
         process.StartInfo.UseShellExecute = false;
+        
         process.Start();
 
         //Collect the output
@@ -51,5 +53,55 @@ public class EndtoEndTest
         {
         File.Delete(testFile);  // only delete if it didn't exist before (just a precaution)
         }
+    }
+    
+    [Fact]
+    
+    public void StoreObservationInDatabase()
+    { //Run the observe function with the message "penguin" and check if was actually stored
+
+        //Same setup as the test above. Declare the test file and check if it exists as well as write the Headers
+        string testFile = "observations.csv";
+        string? backup = null;
+
+        //Just saving the original csv file so we can always return it back to the orignal state after the test.
+        if (File.Exists(testFile))
+        {
+            backup = File.ReadAllText(testFile);  // save original
+        }
+
+        //Overwrite the original csv file with predictible text we can test for
+        File.WriteAllText(testFile, "Id,Author,Timestamp,Message\n");
+
+        //run the correct things in the command line like a user would
+        var process = new Process();
+        process.StartInfo.FileName = "dotnet";
+        process.StartInfo.WorkingDirectory = Directory.GetCurrentDirectory();
+        
+        //again just like in the other test. Build the command like a user would 
+        //with some weird stuff so the directory path is correct.
+        process.StartInfo.Arguments = $"run --project ../../../../../src/Bison.CLI -- observe \"penguin\" ";
+
+        //run command directly and capture what the program prints
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.UseShellExecute = false;
+
+        //run and wait for it to finish
+        process.Start();
+        process.WaitForExit();
+
+        // "is penguin actually in the file we just wrote it to"
+        string fileContent = File.ReadAllText(testFile);
+        Assert.Contains("penguin", fileContent);
+        //"is the username there too?"
+        Assert.Contains(Environment.UserName, fileContent);
+
+        // Same cleanup as in the other test
+        if (backup != null)
+            File.WriteAllText(testFile, backup);
+        else
+            File.Delete(testFile);
+        
+    
     }
 }
