20049459abc061294997be9814c96a3d65737fc2
Julie Lyder Nürnberger <jnyr@itu.dk>
2026-09-17 16:49:13 +0200

    added Location field in Comment

--- src/Bison.CLI/Comment.cs
+++ src/Bison.CLI/Comment.cs
@@ -3,5 +3,6 @@ public record Comment
     int ObservationId, 
     string Author, 
     string Message, 
-    long Timestamp
-): Cheep(Author, Timestamp, Message);
+    long Timestamp,
+    string Location
+): Cheep(Author, Timestamp, Message, Location);
