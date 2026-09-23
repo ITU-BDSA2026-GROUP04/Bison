c3a5dadd6f9b7681dc321aff4a14af79bc55d35b
Julie Lyder Nürnberger <jnyr@itu.dk>
2026-09-23 14:28:12 +0200

    made changes to program.cs so it now takes location in observation

--- src/Bison.CLI/Program.cs
+++ src/Bison.CLI/Program.cs
@@ -15,16 +15,19 @@ var readCommand = new Command("read","read all observations");
 var observeCommand = new Command("observe", "Record an observation");
 var commentCommand = new Command("comment", "Add a comment to an observation");
 var discussionCommand = new Command("discussion", "List all comments for a given observation");
+var locationCommand = new Command("location", "Displays all observation from this location");
 
 var messageArgument = new Argument<string>("message");
 var observationIdArgument = new Argument<int>("observation-id");
 var commentMessageArgument = new Argument<string>("comment-message");
 var discussionIdArgument = new Argument<int>("observation-id");
+var locationArgument = new Argument<string>("Location");
 
 observeCommand.Add(messageArgument);
 commentCommand.Add(commentMessageArgument);
 commentCommand.Add(observationIdArgument);
 discussionCommand.Add(discussionIdArgument);
+locationCommand.Add(locationArgument);
 
 //establishing Root and subcommands "hierarchy"
 //Root command
@@ -46,14 +49,18 @@ readCommand.SetAction((ParseResult parseResult) =>
 //put an observation into the database
 observeCommand.SetAction((ParseResult parseResult) =>
 {
-        string Message = parseResult.GetRequiredValue(messageArgument); //get the message
+        int Id = observationDatabase.Read().Count() + 1; // Works as long as observations are not removed
         string Author = Environment.UserName; //get the author
         long unixTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds(); //get the time
-        int id = observationDatabase.Read().Count() + 1; // Works as long as observations are not removed
-        var observation = new Observation(id, Author, unixTimestamp, Message); //format the observation for storage
+        string[] observationArray = parseResult.GetRequiredValue(observationArgument); //get the message and location
+        string Message = string.Join(" ", observationArray.Take(observationArray.Length - 1)); //taking all elements except for the last in the array and making a string
+        string Location = observationArray[observationArray.Length -1]; //getting the last list, which is the location
+        
+        
+        var observation = new Observation(Id, Author, unixTimestamp, Message, Location); //format the observation for storage
         observationDatabase.Store(observation); //actually store the observation
 
-        UserInterface.PrintObsvervationRecorded(Message); //print conformation using User Interface
+        UserInterface.PrintObsvervationRecorded(Message, Location); //print conformation using User Interface
 });
 
 // comment command
@@ -93,6 +100,33 @@ discussionCommand.SetAction((ParseResult parseResult) =>
 
     IEnumerable<Comment> comments = commentDatabase.Read().Where(c => c.ObservationId == observationId); //get all comments matching the observation Id
     UserInterface.PrintCheeps(comments);
+});
+
+//Location command
+locationCommand.SetAction((ParseResult parseResult) =>
+{
+    string location = parseResult.GetRequiredValue(locationArgument);//get the matching location
+    
+    bool locationExists = false;
+    var observations = new List<Observation>(); //list to hold observations with the location
+    foreach(var o in observationDatabase.Read()) // checks if the location exists and adds to the list
+    {
+        if (o.Location.Equals(location))
+        {
+            locationExists = true;
+            observations.Add(o);
+            
+        }
+        
+    }
+    UserInterface.PrintCheeps(observations); //print the list of observations with this location
+
+    if (!locationExists)
+    {
+        UserInterface.PrintObsvervationRecorded("This location doesn't have any observations", ""); 
+
+    }
+
 });
 
 //parses the input into a parseResult and invokes the action for the command
